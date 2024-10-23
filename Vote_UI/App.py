f035f8949574ce99028e5feac7631cc8c25cd69b
manojkarne <manojkarne22@gmail.com>
2024-10-23 15:43:14 +0530

    Update App.py

--- Vote_UI/App.py
+++ Vote_UI/App.py
@@ -60,7 +60,7 @@ def search_voter():
         for key in list(row_data.keys()):
             if key.startswith('e_'):
                 continue
-            if key.startswith('l_') and 1==1:
+            if key.startswith('l_') and 'address' in key:
                 e_key = 'e_' + key[2:]
                 if e_key in row_data:
                     formatted_data[key[2:]] = f"{row_data[e_key]} / {row_data[key]}"
